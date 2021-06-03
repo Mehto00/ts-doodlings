9605ad5db9e7a319bd360f27134964e0c204692e
Mikko Metso <mikko.metso@sok.fi>
2021-06-03 21:50:43 +0300

    get comments and show some styled card

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { useAxios } from 'use-axios-client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
+http://jsonplaceholder.typicode.com/comments
 interface Comment{
     postId: number,
     id: number,
@@ -9,13 +11,54 @@ interface Comment{
     body: string,
 }
 
+const CommentCard = styled.div`
+  box-sizing: border-box;
+  background-color: #bdbdbd;
+  margin: 1rem 0;
+  padding: 1rem 2rem;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 0.5rem;
+`
+
 const App: React.FC = () => {
-  const { data, error, loading } = useAxios<Comment[]>('http://jsonplaceholder.typicode.com/comments'); return (
+  const defaultComments:Comment[] = [];
+  const [comments, setComments] = useState<Comment[]>(defaultComments);
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
+  const [error, setError]: [string, (error: string) => void] = React.useState("");
+  
+  useEffect(() => {
+    axios.get<Comment[]>("http://jsonplaceholder.typicode.com/comments", {
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(response => {
+      setComments(response.data.slice(0, 5));
+      setLoading(false);
+    }).catch(ex => {
+      const error =
+      ex.response.status === 404
+        ? "Resource Not found"
+        : "An unexpected error has occurred";
+      setError(error);
+      setLoading(false);
+    });
+}, []);
+
+  return (
     <div className="App">
-      <h1>hello world</h1>
       {loading && <div>Loading...</div>}
-      {error && <div>{error.message}</div>}
-      {data && data.map((item) => <p>{item.name}</p>)}
+      {error && <div>{error}</div>}
+      {comments && comments.map((item) => 
+      <CommentCard>
+        <p><strong>{item.name}</strong></p>
+        <p>{item.body}</p>
+        <p>{item.email}</p>
+      </CommentCard>
+      )
+      }
     </div>
   );
 };
